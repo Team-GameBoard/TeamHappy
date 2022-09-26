c40c89f86a7dbc91fe9bd31987ba9a92b6617daf
jsun02 <wotjs1203ab@naver.com>
2022-09-26 10:58:06 +0900

    login기능 추가 및 회원가입

--- TeamHappy/src/user/UserController.java
+++ TeamHappy/src/user/UserController.java
@@ -8,19 +8,69 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 
 @WebServlet("/user")
 public class UserController extends HttpServlet {
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		User.builder().user_age(11).build();
-		
-		//		try {
-//			UserDAO.writeUser(new User("asdasd123", "asdasd123", 5));
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-	
-	}
+   protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  
+	   // 커맨드 패턴
+	   String command = request.getParameter("command");
+	   
+	   if(command == null) {
+		   command = "user";
+	   }else if(command.equals("usercr")){
+		   // 회원가입
+		   String user_id = request.getParameter("user_id");
+		   String user_password = request.getParameter("user_password");
+		   int user_age = Integer.parseInt(request.getParameter("user_age"));
+		   
+		   System.out.println("user_id : " + user_id);
+		   System.out.println("user_password : " + user_password);
+		   System.out.println("user_age : " + user_age);
+		   
+		   try {
+			   UserDAO.writeUser(new User(user_id,user_password,user_age));
+		   } catch (SQLException e) {
+			   e.printStackTrace();
+		   }
+	   }else if(command.equals("userlogin")) {
+		   // 로그인
+		   String id = request.getParameter("id");
+		   String pw = request.getParameter("pw");
+		   
+		   System.out.println("id : " +id);
+		   System.out.println("pw : " +pw);
+		   
+		   request.setAttribute("id", id);
+		   request.setAttribute("pw", pw);
+		   
+		   User user = new User(id,pw);
+		   User userLogin = new User();
+		   try {
+			   userLogin = UserDAO.Login(user);
+			   HttpSession session = request.getSession();
+			   if(userLogin != null) {
+				   session.setAttribute("id", userLogin.getUser_id());
+				   session.setAttribute(pw, userLogin.getUser_password());
+				   request.getRequestDispatcher("Logintest.jsp").forward(request, response);
+			   }else {
+//				   response.sendRedirect("Login.jsp");
+				   request.getRequestDispatcher("Login.jsp").forward(request, response);
+			   }
+			   
+		   }catch (Exception e) {
+			// TODO: handle exception
+			   e.printStackTrace();
+		}
+		   
+		  
+	   }
+      
+	   
+	   
+	   
+   }
 
 }
--- TeamHappy/src/user/UserDAO.java
+++ TeamHappy/src/user/UserDAO.java
@@ -2,37 +2,136 @@ package user;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
 
 import util.DBUtil;
 
 public class UserDAO {
-	// 게시물 등록
-	// Query
-	// VO Value Object(set메소드가 없는 객체, 읽기 전용)
-	public static boolean writeUser(User user) throws SQLException {
-		Connection con = null;
-		PreparedStatement pstmt = null;
-		boolean result = false;
-
-		System.out.println(user);
-
-		try {
-			con = DBUtil.getConnection();
-			pstmt = con.prepareStatement("insert into user (user_id, user_password, user_age) values (?,?,?)");
-
-			pstmt.setString(1, user.getUser_id());
-			pstmt.setString(2, user.getUser_password());
-			pstmt.setInt(3, user.getUser_age());
-
-			int count = pstmt.executeUpdate();
-
-			if (count != 0) {
-				result = true;
-			}
-		} finally {
-			DBUtil.close(con, pstmt);
+   
+   
+   // 아이디 중복값 확인
+//   public boolean confimId(User user) {
+//      boolean result = false;
+//      Connection con = null;
+//      PreparedStatement pstmt = null;
+//      ResultSet rset = null;
+//      
+//      try {
+//    	 con = DBUtil.getConnection();
+//         pstmt = con.prepareStatement("select user_id from user where user_id =?");
+//         pstmt.setString(1, user.getUser_id());
+//         rset = pstmt.executeQuery();
+//         
+//         if(rset.next()) {
+//        	if(rset.getString(1).equals(user.getUser_id())) {
+//        		System.out.println("중복된 아이디 입니다.");
+//        	}else {
+//        		System.out.println("아이디 생성 가능");
+//        	}
+//         }
+//      }catch (Exception e) {
+//         e.printStackTrace();
+//      }finally {
+//         DBUtil.close(con, pstmt, rset);
+//      }
+//      return result;
+//   }
+	
+	public static void Loginck(User user) {
+		   Connection con = null;
+		   PreparedStatement pstmt = null;
+		   ResultSet rset = null;
+		   
+		   try {
+			   con = DBUtil.getConnection();
+		       pstmt = con.prepareStatement("select user_id from user where user_id =?");
+		       
+		       pstmt.setString(1, user.getUser_id());
+		       rset = pstmt.executeQuery();
+		       if(rset.next()) {
+		    	if(rset.getNString(1).equals(user.getUser_id())){
+		    		System.out.println("중복된 아이디 값");
+		    	} 
+		      }else {
+		    	  System.out.println("아이디 생성 가능");
+		      }
+		   } catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			   DBUtil.close(con, pstmt, rset);
 		}
-		return result;
+		   
+	   }
+	
+	
+	
+   
+   
+   // 회원가입 등록
+   // Query
+   public static boolean writeUser(User user) throws SQLException {
+      Connection con = null;
+      PreparedStatement pstmt = null;
+      
+      boolean result = false;
+
+      System.out.println(user);
+
+      try {
+         con = DBUtil.getConnection();
+         pstmt = con.prepareStatement("insert into user (user_id, user_password, user_age) values (?,?,?)");
+
+         pstmt.setString(1, user.getUser_id());
+         pstmt.setString(2, user.getUser_password());
+         pstmt.setInt(3, user.getUser_age());
+
+         int count = pstmt.executeUpdate();
+
+         if (count != 0) {
+            result = true;
+         }
+      } finally {
+         DBUtil.close(con, pstmt);
+      }
+      return result;
+   }
+   
+   
+   // 로그인 로직
+   public static User Login(User user) {
+	   Connection con = null;
+	   PreparedStatement pstmt = null;
+	   ResultSet rset = null;
+	   User reUser = new User();
+	   
+	   try {
+		   con = DBUtil.getConnection();
+	       pstmt = con.prepareStatement("select * from user where user_id=? and user_password=?");
+	       
+	       pstmt.setString(1, user.getUser_id());
+	       pstmt.setString(2, user.getUser_password());
+	       rset = pstmt.executeQuery();
+	       if(rset.next()) {
+	    	if(rset.getNString(1).equals(user.getUser_password())){
+	    		System.out.println("로그인 성공");
+	    		reUser = new User(rset.getString(1),rset.getString(2));
+	    		return reUser;
+	    	} 
+	      }else {
+	    	  System.out.println("로그인 실패");
+	      }
+	   } catch (SQLException e) {
+		e.printStackTrace();
+	}finally {
+		   DBUtil.close(con, pstmt, rset);
 	}
+	return null;
+	   
+   }
+   
+   
 }
